32d5f897a6a5e88d180e6314838591c1e8d07a7e
manarnoldi <manarnoldi@gmail.com>
2019-07-31 17:04:21 +0300

    Updated the crystal reports removing subjects columns leaving eng,kis,mat,sci,soc

--- School Soft/My Project/Settings.Designer.vb
+++ School Soft/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -92,7 +92,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("KEKLF-LT0944-L\SQLEXPRESS")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("DESKTOP-FV1KTBF")>  _
         Public Property serverName() As String
             Get
                 Return CType(Me("serverName"),String)
